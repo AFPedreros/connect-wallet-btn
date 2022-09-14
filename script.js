66b7285b6da55f6ade037b255fbaacc0a6a0d2ab
AFPedreros <felipe.pedreros94@gmail.com>
2022-09-14 09:27:46 -0500

    refactoring

--- script.js
+++ script.js
@@ -3,45 +3,6 @@ const circle = document.createElement("span")
 let localAccount = []
 
 const main = async () => {
-    const animationEffect = (e) => {
-        const x = e.clientX
-        const y = e.clientY
-        const buttonTop = e.target.offsetTop
-        const buttonLeft = e.target.offsetLeft
-
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
-
-        circle.style.top = yInside + "px"
-        circle.style.left = xInside + "px"
-
-        e.target.appendChild(circle)
-
-        setTimeout(() => {
-            circle.remove()
-        }, 10000)
-    }
-
-    const connectWallet = async () => {
-        try {
-            const { ethereum } = window
-
-            const accounts = await ethereum.request({
-                method: "eth_requestAccounts",
-            })
-
-            localAccount = accounts[0]
-            window.location.reload()
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const isMetaMaskInstalled = () => {
-        const { ethereum } = window
-        return Boolean(ethereum && ethereum.isMetaMask)
-    }
-
     const MetaMaskClientCheck = () => {
         if (!isMetaMaskInstalled()) {
             walletBtn.innerText = "Click here to install MetaMask!"
@@ -52,10 +13,6 @@ const main = async () => {
         } else {
             isWalletConnected()
         }
-    }
-
-    const test = () => {
-        console.log("hello")
     }
 
     const isWalletConnected = async () => {
@@ -88,6 +45,49 @@ const main = async () => {
         } catch (error) {
             console.log("error: ", error)
         }
+    }
+
+    const isMetaMaskInstalled = () => {
+        const { ethereum } = window
+        return Boolean(ethereum && ethereum.isMetaMask)
+    }
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window
+
+            const accounts = await ethereum.request({
+                method: "eth_requestAccounts",
+            })
+
+            localAccount = accounts[0]
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const test = () => {
+        console.log("hello")
+    }
+
+    const animationEffect = (e) => {
+        const x = e.clientX
+        const y = e.clientY
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        circle.style.top = yInside + "px"
+        circle.style.left = xInside + "px"
+
+        e.target.appendChild(circle)
+
+        setTimeout(() => {
+            circle.remove()
+        }, 10000)
     }
 
     MetaMaskClientCheck()
